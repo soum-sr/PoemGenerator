d6239a1f7d4850bf2e3992a4fe911610c023167f
soum-sr <soum1071@gmail.com>
2020-05-24 18:03:58 +0530

    added index.html

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask 
-
+from flask import render_template
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def index():
-	return "Poem Generator"
+	return render_template('index.html')
 
 if __name__ == '__main__':
 	app.run(debug=True)
