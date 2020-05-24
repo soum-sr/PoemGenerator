27a57b2bfaa11bf813880bd3329782a4319f4101
soum-sr <soum1071@gmail.com>
2020-05-24 18:14:28 +0530

    updated code

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask 
-from flask import render_template
+from flask import render_template, request
 app = Flask(__name__)
+
 
 
 @app.route('/', methods=['GET','POST'])
 def index():
+	if request.method == "POST":
+		sentence = request.form['line_input']
+		output = 
 	return render_template('index.html')
 
 if __name__ == '__main__':
